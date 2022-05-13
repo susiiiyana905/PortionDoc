0fb4f5e2cabfddde28f09f2673883aebcf7f705e
sadixyastha <shtsadikshya28@gmail.com>
2022-05-13 17:14:34 +0545

    Update Signupform.js

--- src/Components/User/Signupform.js
+++ src/Components/User/Signupform.js
@@ -17,68 +17,38 @@ const SignupForm = () => {
             <h1 style={{ color: "black", textAlign:"center", fontSize:"60px", fontFamily:"sans-serif"  }}>Create an Account</h1>
           {/* </div> */}
             <form>
-<<<<<<< HEAD
-        <div class="form-row">
-          <div class="form-group col-md-6">
-            <label for="inputFirstname4">First Name</label>
-            <input type="First Name" class="form-control" id="inputFirstname4"/>
+
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="inputFirstname4">First Name</label>
+              <input type="First Name" class="form-control" id="inputFirstname4"/>
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputLastname4">Last Name</label>
+              <input type="Last Name" class="form-control" id="inputLastname4"/>
+            </div>
           </div>
-          <div class="form-group col-md-6">
-            <label for="inputLastname4">Last Name</label>
-            <input type="Last Name" class="form-control" id="inputLastname4"/>
+          <div class="form-group">
+            <label for="inputEmail">Email</label>
+            <input type="text" class="form-control" id="inputEmail"/>
           </div>
-        </div>
-        <div class="form-group">
-          <label for="inputEmail">Email</label>
-          <input type="text" class="form-control" id="inputEmail"/>
-        </div>
-        <div class="form-group">
-          <label for="inputAddress2">Password</label>
-          <input type="text" class="form-control" id="inputPassword2"/>
-        </div>
-        <div class="form-group">
-          <label for="inputAddress2"> Confirm Password</label>
-          <input type="text" class="form-control" id="inputConfirmPassword2"/>
-        </div>
-      
+          <div class="form-group">
+            <label for="inputAddress2">Password</label>
+            <input type="text" class="form-control" id="inputPassword2"/>
+          </div>
+          <div class="form-group">
+            <label for="inputAddress2"> Confirm Password</label>
+            <input type="text" class="form-control" id="inputConfirmPassword2"/>
+          </div>
+        
 
-        <button type="submit" className="btn btn-primary sign-btn" style = {{"background-color":'#FF7800' }} >Sign Up</button>
+          <button type="submit" className="btn btn-primary sign-btn" style = {{"background-color":'#FF7800' }} >Sign Up</button>
 
-      </form>
-      </div>
-=======
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputFirstname4">First Name</label>
-      <input type="First Name" class="form-control" id="inputFirstname4"/>
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputLastname4">Last Name</label>
-      <input type="Last Name" class="form-control" id="inputLastname4"/>
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputEmail">Email</label>
-    <input type="text" class="form-control" id="inputEmail"/>
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2">Password</label>
-    <input type="text" class="form-control" id="inputPassword2"/>
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2"> Confirm Password</label>
-    <input type="text" class="form-control" id="inputConfirmPassword2"/>
-  </div>
- 
-
-  <button type="submit" className="btn btn-primary sign-btn" style = {{"background-color":'#FF7800' }} >Sign Up</button>
-
-</form>
-</div>
->>>>>>> e7a8ed665ac5714df92b865c239ebf6c1cdd830f
-        </>
-    )
-    
-    
-}
+        </form>
+        </div>
+                </>
+            )
+            
+            
+        }
 export default SignupForm;
