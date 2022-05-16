e109383797710846539e051916b21e8f7a689e96
krisaa <57648393+krisaa@users.noreply.github.com>
2022-05-16 07:34:55 +0545

    updatess

--- src/Components/User/login.js
+++ src/Components/User/login.js
@@ -1,63 +1,89 @@
-import { Component } from "react";
-
-class Login extends Component{
-    render(){
+import axios from "axios";
+import { useState } from "react";
+// import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+const Login =()=> {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const userLogin = (e)=>{
+        e.preventDefault();
+        if(email.trim()==="") {
+            setMessage("Email is required.");
+            return;            
+        } else if (password.trim()==="") {
+            setMessage("Password is required.");
+            return;                        
+        } 
+        const userData = {
+            email, password
+        }
+        axios.post("http://localhost:4001/user/login", userData)
+        .then(result1=>{
+            if(result1.data.token){
+                //login success
+                localStorage.setItem('token', result1.data.token)
+                console.log(result1.data);
+                // window.location.replace('/');
+            }
+            else{
+                //login failed
+                setMessage(result1.data.message)
+            }
+        })
+        .catch(e)
+    }
         return(
             <> 
-                 <nav class="navbar navbar-expand-lg ">
-               
-               <i class="fas fa-solid fa-envelope fa-lg" style={{height: "40px", color:"white"}}></i><p className="i-1">portiondoc@gmail.com</p>
-               
-               <i class="fas fa-solid fa-phone" style={{height: "40px", marginLeft:"100px", color:"white"}} ></i><p className="i-1">+977 983142567</p>
-               </nav>
-            <div>
-
-                 <div className="col-md-6 d-flex justify-content-center mx-auto ">
-
-               <div className="container">
-                <div className="row">
-                <div className="col-md-4">
-                    
-               
-                    </div>
-                    <div className="col-md-4">
-                 <img src="images/logo.png" className="log"></img>
-
-                 <form>
-                     
-                        <div className="form-group mt-4">
-                            <label>Email</label>
-                            <input type="text" className="form-control" style={{width:"600px"}}/>
-                        </div>
-
-                        <div className="form-group mt-3">
-                            <label>Password</label>
-                            <input type="password" className="form-control" style={{width:"600px"}}/>
-                            
-                        </div>
-
-                        <div>
-                            <div class = "form-group form-check mt-2">
-                                <input type ="checkbox" class="form-check-input" id="rememberme"></input>
-                                <label class = "form-check-label" for="rememberme">Remember Me</label>
+                <nav className="navbar navbar-expand-lg ">         
+                    <i className="fas fa-solid fa-envelope fa-lg" style={{height: "40px", color:"white"}}></i><p className="i-1">portiondoc@gmail.com</p>     
+                    <i className="fas fa-solid fa-phone" style={{height: "40px", marginLeft:"100px", color:"white"}} ></i><p className="i-1">+977 983142567</p>
+                </nav>
+                <div>
+                    <div className="col-md-6 d-flex justify-content-center mx-auto ">
+                        <div className="container">
+                        <p style={{color:"red", fontWeight:"bold", marginTop:"50px", textAlign:"center"}}>{message}</p>
+                            <div className="row">
+                                <div className="col-md-4">
+                                    <img src="images/logo.png" className="log"></img>
+                                    <form>
+                                        <div className="form-group mt-4">
+                                            <label>Email</label>
+                                            <input type="text" className="form-control" style={{width:"600px"}}
+                                            value={email}
+                                            onChange={(e)=>setEmail(e.target.value)}
+                                            />
+                                        </div>
+                                        <div className="form-group mt-3">
+                                            <label>Password</label>
+                                            <input type="password" className="form-control" style={{width:"600px"}}
+                                            value={password}
+                                            onChange={(e)=>setPassword(e.target.value)}
+                                            />
+                                        </div>
+                                        <div>
+                                            <div className = "form-group form-check mt-2">
+                                                <input type ="checkbox" className="form-check-input" id="rememberme"></input>
+                                                <label className = "form-check-label" for="rememberme">Remember Me</label>
+                                            </div>
+                                        </div>
+                                        <div className="form-group">
+                                            <button type="Submit" className="btn btn-light login"  style={{width:'fit-content'}}
+                                            onClick={userLogin}
+                                            >Login</button>
+                                        </div>
+                                    </form>
+                                    <div className="card mt-4 border-0" style={{width:"25rem"}}>
+                                        <div className="card-body">
+                                            <div className="text-center">Don't have an account? <Link className="link" to="/signup">SignUp</Link></div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-
-                        <div className="form-group">
-
-                            <button type="Submit" className="btn btn-light login"  style={{width:'fit-content'}}>Login</button>
-
-                        </div>
-                    </form>
-                    {/* </div> */}
                     </div>
-                    </div>
-                    </div>
-
-            </div>
+                </div>
             </>
         )
-    }
 }
-
-export default Login;
+export default Login;;
--- src/Components/header.js
+++ src/Components/header.js
@@ -1,35 +1,18 @@
 import { Component } from "react";
+import { Link } from "react-router-dom";
 // import { Link } from "react-router-dom";
-
 class Header extends Component{
     render(){
         return(
             <div>
-                <nav class="navbar navbar-expand-lg ">
-
-                 <i class="fa-solid fa-envelope" style={{height: "40px"}}>portiondoc@gmail.com</i>
-                 <i class="fa-solid fa-phone" style={{height: "40px", float:"right"}} >+977 983142567</i>
-                 </nav>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="#"><img src="images/logo.png" className="card-img-top" alt="..." style={{height:"80px"}}></img></a>
-                    {/* <img src="images/logo.png" className="card-img-top" alt="..." ></img> */}
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse nav" id="navbarNav">
-                        <ul className="navbar-nav">
-
-               
-                 <i class="fas fa-solid fa-envelope fa-lg" style={{height: "40px", color:"white"}}></i><p className="i-1">portiondoc@gmail.com</p>
-                 
-                 <i class="fas fa-solid fa-phone" style={{height: "40px", marginLeft:"100px", color:"white"}} ></i><p className="i-1">+977 983142567</p>
-                 </nav>
+                   <nav class="navbar navbar-expand-lg ">
+               <i class="fas fa-solid fa-envelope fa-lg" style={{height: "40px", color:"white"}}></i><p className="i-1">portiondoc@gmail.com</p>
+               <i class="fas fa-solid fa-phone" style={{height: "40px", marginLeft:"100px", color:"white"}} ></i><p className="i-1">+977 983142567</p>
+               </nav>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <a className="navbar-brand" href="#"><img src="images/logo.png" className="card-img-top" alt="..." style={{height:"100px", width:"150px", marginLeft:"100px"}}></img></a>
                     <div class="collapse navbar-collapse" style={{marginLeft:"400px"}} id="navbarSupportedContent">
                         <ul className="navbar-nav nav-item">
-
-
                         <li className="nav-item active">
                             <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                         </li>
@@ -45,28 +28,13 @@ class Header extends Component{
                         <li className="nav-item">
                             <a className="nav-link">Contact Us</a>
                         </li>
-
-                        <li><button className="btn btn-nav">Sign Up</button></li>
-                    
+                        <button className="btn-sign"><Link className="link" to="/login" style={{color: "white", textDecoration:"none"}}>Login</Link></button>
                         </ul>
-                        
                     </div>
-
-
-                       <button className="btn-sign">Sign Up</button>
-
-                    
-                        </ul>
-                        </div>
-                        
-                    
-
-                    
+                    {/* </div> */}
                     </nav>
-
-            </div>
+                    </div>
         )
     }
 }
-
 export default Header;
