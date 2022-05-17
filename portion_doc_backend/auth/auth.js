1e89c91899361901ca1050aa43b8c3a89abd28da
susiiiyana905 <70754133+susiiiyana905@users.noreply.github.com>
2022-05-17 22:09:34 +0545

    view and update user profile

--- portion_doc_backend/auth/auth.js
+++ portion_doc_backend/auth/auth.js
@@ -3,23 +3,28 @@ const User = require("../models/userModel");
 
 
 //guard for user
-module.exports.verifyUser = function(req,res,next){
-    try{
+module.exports.verifyUser = function (req, res, next) {
+    try {
         const token = req.headers.authorization.split(" ")[1];
+        //console.log(token);
         const uData = jwt.verify(token, "loginKey");
-        //select * from users where _id = udata.userID
-        User.findOne({_id: uData.userID})
-        .then(function(userData){
-            // console.log(userData)
-            req.userInfo = userData;
-            next();
+        // console.log("try");
+        User.findOne({ _id: uData.userId })
+        .then(function (userData) {
+            if (userData.admin == false) {
+                req.userInfo = userData;
+                next();
+            } else {
+                res.json({ message: "Users not allow" });
+            }
         })
-        .catch(function(e){
-            res.json({error : e.message})
-        })
+            .catch(function (e) {
+                res.json({ error: e })
+            })
     }
-    catch(e){
-        res.json({error : e.message})
+    catch (e) {
+        console.log("catch");
+        res.json({ error: e })
     }
 }
 
