902a9e0ba4c5e543ceba22805c5ccb62cc8e7a82
sadixyastha <shtsadikshya28@gmail.com>
2022-05-28 15:36:22 +0545

    updated format of add ingredient

--- src/Components/admin/add.js
+++ src/Components/admin/add.js
@@ -3,6 +3,40 @@ import { useState } from "react";
 import { BrowserRouter as Router, Link, useNavigate } from "react-router-dom";
 
 const AddMeal =()=> {
+
+    // constructor(props)
+    //  {
+    //     super(props)
+    //     this.state = { 
+    //        formValues: [{ ingredient: ""}]
+    //      };
+    //     this.handleSubmit = this.handleSubmit.bind(this)
+    //   }
+      
+    //   handleChange(i, e)
+    //    {
+    //     let formValues = this.state.formValues;
+    //     formValues[i][e.target.ingredient] = e.target.value;
+    //     this.setState({ formValues });
+    //   }
+    
+    //   addFormFields() {
+    //     this.setState(({
+    //       formValues: [...this.state.formValues, { ingredient: "" }]
+    //     }))
+    //   }
+    
+    //   removeFormFields(i) {
+    //     let formValues = this.state.formValues;
+    //     formValues.splice(i, 1);
+    //     this.setState({ formValues });
+    //   }
+    
+    //   handleSubmit(event) {
+    //     event.preventDefault();
+    //     alert(JSON.stringify(this.state.formValues));
+    //   }
+      
     const [mealImage, setMealImage] = useState('');
     const [mealName, setMealName] = useState('');
     const [mealPrice, setMealPrice] = useState('');
@@ -104,7 +138,10 @@ const AddMeal =()=> {
         <div class="form-group row">
             <label class="col-sm-2 col-form-label">Meal Description</label>
             <div class="col-sm-10">
-            <textarea type="text" class="form-control"></textarea>
+            <textarea type="text" class="form-control" 
+             value={mealDescription}
+             onChange={(e)=>setMealDescription(e.target.value)}
+            ></textarea>
             </div>
         </div>
         <div class="form-group row">
@@ -169,113 +206,6 @@ const AddMeal =()=> {
   </form>
   </div>
         </>
-        // <div>
-        // <div className="con">
-        //     <div className="row">
-        //         <div className="col-md-4"></div>
-        //         <div className="col-md-4">
-        //             <h2 className="heading-h2-all">Add Meal:</h2>
-        //             <form>
-        //                 <div className="form-group row ">
-        //                     <label class="col-sm-2 col-form-label">Meal Name</label>
-        //                     <div class="col-sm-10">
-        //                     <input type="text" className="form-control"
-        //                     value={mealName}
-        //                     onChange={(e)=>setMealName(e.target.value)}
-        //                     />
-        //                     </div>
-        //                 </div>
-                        
-
-        //                 <div className="form-group row">
-        //                     <label>Meal Price</label>
-        //                     <div class="col-sm-10">
-        //                     <input type="text" className="form-control"
-        //                      value={mealPrice}
-        //                      onChange={(e)=>setMealPrice(e.target.value)}
-        //                     />
-        //                     </div>
-        //                 </div>
-
-        //                 <div className="form-group row">
-                        
-        //                 <label for="inputCategory" className="col-form-label">Meal Category</label>
-        //                 <div class="col-sm-10">
-                            
-        //                         <select className="custom-select custom-select-lg" style={{width:"100%"}}
-        //                         value={mealCategory}
-        //                         onChange={e=>setMealCategory(e.target.value)}
-        //                         >
-        //                             <option value="Veg">Veg</option>
-        //                             <option value="Non-Veg">Non-Veg</option>
-        //                             <option value="Vegan">Vegan</option>
-        //                         </select>
-        //                     </div>
-        //                     </div>
-
-        //                     <div className="form-group row">
-        //                     <label>Meal Description</label>
-        //                     <div class="col-sm-10">
-        //                     <textarea type="text" className="form-control"
-        //                      value={mealDescription}
-        //                      onChange={(e)=>setMealDescription(e.target.value)}
-                            
-        //                     />
-        //                     </div>
-        //                     </div>
-
-        //                     <div className="form-group row">
-        //                     <label>Time</label>
-        //                     <div class="col-sm-10">
-        //                     <input type="text" className="form-control"
-        //                      value={time}
-        //                      onChange={(e)=>setTime(e.target.value)}
-        //                     />
-        //                     </div>
-        //                     </div>
-
-        //                     <div className="form-group row">
-        //                     <label>Calory</label>
-        //                     <div class="col-sm-10">
-        //                     <input type="text" className="form-control"
-        //                      value={calory}
-        //                      onChange={(e)=>setCalory(e.target.value)}
-                            
-        //                     />
-        //                     </div>
-        //                     </div>
-
-        //                     <div className="form-group row">
-        //                     <label for="inputDifficulty" className="col-form-label">Difficulty</label>
-        //                     <div class="col-sm-10">
-        //                     <select className="custom-select custom-select-lg" style={{width:"100%"}}
-        //                         value={difficulty}
-        //                         onChange={e=>setDifficulty(e.target.value)}
-        //                         >
-        //                             <option value="Difficult">Difficult</option>
-        //                             <option value="Medium">Medium</option>
-        //                             <option value="Easy">Easy</option>
-        //                         </select>
-        //                     </div>
-        //                     </div>
-
-        //                 <div className="form-group">
-        //                     <label>Meal Image</label>
-        //                     <input type="file" className="form-control"
-        //                     onChange={(e)=>setMealImage(e.target.files[0])}
-        //                     />
-
-        //                 </div>
-
-        //                 <p><button type='submit' className="btn btn-primary"
-        //                 onClick={addMeal}
-        //                 >Add</button></p>
-        //             </form>
-        //         </div>
-        //         <div className="col-md-4"></div>
-        //     </div>
-        // </div>
-        // </div>
     )
     
 }
