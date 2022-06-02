4880dce031e8d6f4e2c42f1dc42a4a42b7c0b175
sadixyastha <shtsadikshya28@gmail.com>
2022-06-02 13:43:24 +0545

    Review

--- portion_doc_backend/router/reviewRoute.js
+++ portion_doc_backend/router/reviewRoute.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const router = new express.Router();
 const auth = require("../auth/auth");
+const Category = require("../models/categoryModel");
 const Review = require("../models/reviewModel");
 
 router.post("/add/review", auth.verifyUser, async(req,res)=>{
     const subject = req.body.subject;
-    const message = req.body.message;
+    const reviewMessage = req.body.reviewMessage;
 
     const reviewData = new Review({
         subject: subject,
-        message: message,
-        user_id: req.userInfo._id
+        reviewMessage: reviewMessage,
+        user_id: req.userInfo._id,
     })
     reviewData.save()
-    res.json({ success: true, message: "Review send successfully" });
-    // .then(function(){
-       
-    //     res.status(200).send({ success: true, message:"Review send Successfully"})
-    // }).catch(function(e){
-    //     res.status(400).send({message: e})
+ 
+    .then(function(){
+        res.status(200).send({ success: true, message:"Review send Successfully"})
+    }).catch(function(e){
+        res.status(400).send({message: e})
     
-    // })
+    })
 })
 
 router.get("/get/reviews", auth.verifyUser, function(req, res){
@@ -34,4 +34,11 @@ router.get("/get/reviews", auth.verifyUser, function(req, res){
     })
 })
 
+router.get("/get/all/reviews",auth.verifyAdmin, async(req,res)=>{
+    const reviewData = await Review.find()
+    .populate("user_id","firstName lastName profile_pic")
+    .sort({createdAt:-1});
+    res.json({success: true, message:"Review Data", data:reviewData});
+
+})
 module.exports = router
