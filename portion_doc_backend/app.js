0afd2938f090a0691e6ec2bfa39cedd9a46f4e38
Bikesh7 <77.bikesh@gmail.com>
2022-07-05 20:49:21 +0545

    view and place order

--- portion_doc_backend/app.js
+++ portion_doc_backend/app.js
@@ -41,9 +41,8 @@ app.use(ReviewRoute);
 const OrderRoute = require("./router/orderRoute");
 app.use(OrderRoute);
 
-
-const CartRoute = require("./router/cartRoute");
-app.use(CartRoute);
+// const CartRoute = require("./router/cartRoute");
+// app.use(CartRoute);
 
 const PreferenceCategory = require("./router/dietCatogaryRoute");
 app.use(PreferenceCategory);
