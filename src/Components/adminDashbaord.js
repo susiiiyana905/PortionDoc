2ef41a73422e151da904ca0411bda9fd3abd9cca
sadixyastha <shtsadikshya28@gmail.com>
2022-07-09 19:13:30 +0545

    Final commit

--- src/Components/adminDashbaord.js
+++ src/Components/adminDashbaord.js
@@ -101,11 +101,10 @@ const AdminDashboard = ({children}) => {
                 <div className="dropdown pb-4">
                     <Link to="/" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                         <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" className="rounded-circle" />
-                        <span className="d-none d-sm-inline mx-1">loser</span>
+                        <span className="d-none d-sm-inline mx-1">Sadikshya</span>
                     </Link> 
                     <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
-                        <li><a className="dropdown-item" href="/">New project...</a></li>
-                        <li><a className="dropdown-item" href="/">Settings</a></li>
+
                         <li><a className="dropdown-item" href="/">Profile</a></li>
                         <li>
                             <hr className="dropdown-divider" />
